85212f29da8293bfa3d1b5680d2d067680af7c02
LY_D4C <249503039@qq.com>
2022-07-07 18:28:44 +0800

    [fix] add styled

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,6 +3,16 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     grey1: string;
+    grey2: string;
+    grey3: string;
+    grey4: string;
+    grey5: string;
+    fontLargest: string;
+    fontLarge: string;
+    fontNormal: string;
+    fontSmall: string;
+    fontSmaller: string;
     primaryColor: string;
+    borderColor: string;
   }
 }
