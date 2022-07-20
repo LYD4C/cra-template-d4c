da3ca7ce0c416d7b9c4610c8a129714f9eda9987
mcl <1120664311@qq.com>
2022-07-20 16:32:18 +0800

    feat: eslint 工具

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
-  extends: ['eslint-config-ali/typescript/react'],
+  extends: [
+    'eslint-config-ali/typescript/react',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   rules: {
-    semi: 'off',
-    '@typescript-eslint/semi': ['error', 'never'],
-    'react/prop-types': [0],
-    'arrow-parens': [2, 'as-needed'],
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/consistent-type-assertions': ['warn', { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter'}],
+    '@typescript-eslint/no-require-imports': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react/prop-types': 'off',
   },
-}
+};
